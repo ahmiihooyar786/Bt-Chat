26f91f2e9a31f2c8f1e80da13bec9936a0c5ef0e
ahmiihooyar786 <ahmadnadeemq2@gmail.com>
2026-05-04 17:54:15 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAY7r_iIEoB5Kz42jfJ5P2wE8fKrZs5Mw8",
+    authDomain: "ahmad-rdx-toolkit.firebaseapp.com",
+    databaseURL: "https://ahmad-rdx-toolkit-default-rtdb.firebaseio.com",
+    projectId: "ahmad-rdx-toolkit",
+    storageBucket: "ahmad-rdx-toolkit.firebasestorage.app",
+    messagingSenderId: "1016906380254",
+    appId: "1:1016906380254:web:9944d39e4806fedfc7e34f"
+};
